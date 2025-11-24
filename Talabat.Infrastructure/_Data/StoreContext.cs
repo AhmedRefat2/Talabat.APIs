109c0c12409f328a9d5c9ae65b130b95014a2e03
Ahmed Refaat 3 <ahmed202203195@gmail.com>
2025-11-24 17:30:44 +0200

    feat(infrastructure): Add DbContext and Migrations

---
+++ Talabat.Infrastructure/_Data/StoreContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities.Product;
+
+namespace Talabat.Repository.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Fluent API Configuration 
+            //modelBuilder.ApplyConfiguration(new ProductConfig());
+            //modelBuilder.ApplyConfiguration(new ProductBrandConfigs());
+            //modelBuilder.ApplyConfiguration(new ProductCategoryConfigs());
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());   
+        }
+
+        #region Db Sets
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; } 
+        #endregion
+
+    }
+}
