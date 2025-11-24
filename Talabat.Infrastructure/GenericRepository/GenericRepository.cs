87094a2645aed5870abdf765ddb43ffa9344b8dd
Ahmed Refaat 3 <ahmed202203195@gmail.com>
2025-11-24 18:07:27 +0200

    feat(Infrastructure): add GenericRepo Implementation

---
+++ Talabat.Infrastructure/GenericRepository/GenericRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+using Talabat.Core.Repositories.Contract;
+using Talabat.Repository.Data;
+
+namespace Talabat.Infrastructure.GenericRepository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        private readonly StoreContext _dbContext;
+
+        public GenericRepository(StoreContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IReadOnlyList<T>> GetAllAsync()
+             => await _dbContext.Set<T>().ToListAsync();
+
+        public async Task<T?> GetAsync(int id)
+            => await _dbContext.Set<T>().FindAsync(id); // FindAsync is optimized for primary key lookups 
+
+
+        //public void Add(T entity)
+        //{
+        //    _dbContext.Add(entity);
+        //    _dbContext.SaveChangesAsync();
+        //}
+
+        //public void Update(T entity)
+        //    => _dbContext.Update(entity);
+
+        //public void Delete(T entity)
+        //    => _dbContext.Remove(entity);
+
+
+    }
+}
