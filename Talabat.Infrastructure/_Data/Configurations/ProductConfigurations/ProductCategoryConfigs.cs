be7d21a8e70420f396c5632aa4b2f9b5fcb0d8c4
Ahmed Refaat 3 <ahmed202203195@gmail.com>
2025-11-24 16:39:08 +0200

    feat(domain): add Product, Category, Brand Configurations

---
+++ Talabat.Infrastructure/_Data/Configurations/ProductConfigurations/ProductCategoryConfigs.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities.Product;
+
+namespace Talabat.Infrastructure._Data.Configurations.ProductConfigurations
+{
+    internal class ProductCategoryConfigs : IEntityTypeConfiguration<ProductCategory>
+    {
+        public void Configure(EntityTypeBuilder<ProductCategory> category)
+        {
+            category.Property(c => c.Name).IsRequired();
+        }
+    }
+  
+}
